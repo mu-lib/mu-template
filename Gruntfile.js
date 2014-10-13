a9984a0ca3dfd1c6869775c3d2c76dbdd78e8ca9
Mikael Karon <mikael@karon.se>
2014-10-13 15:08:51 +0800

    Micro updates.

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,20 +9,7 @@
 module.exports = function(grunt) {
 	"use strict";
 
-	var UNDEFINED;
-
 	grunt.initConfig({
-		"pkg" : grunt.file.readJSON("package.json"),
-
-		"build" : {
-			"src" : ".",
-			"dist" : "dist",
-			"banner" : "/**\n" +
-				" * <%= pkg.name %> - <%= pkg.version %>\n" +
-				" * @license <%= pkg.licenses[0].type %> <%= pkg.licenses[0].url %> © <%= pkg.author.name %> mailto:<%= pkg.author.email%>\n" +
-				" */"
-		},
-
 		"jshint": {
 			"all": [
 				"Gruntfile.js",
@@ -33,80 +20,13 @@ module.exports = function(grunt) {
 			}
 		},
 
-		"clean" : [ "<%= build.dist %>"],
-
-		"copy" : {
-			"all" : {
-				"files" : [{
-					"expand" : true,
-					"cwd" : "<%= build.src %>",
-					"dest" : "<%= build.dist %>",
-					"src" : [ "bower.json", "package.json", "main.js", "plugin.js" ]
-				}]
-			}
-		},
-
-		"git-describe" : {
-			"all" : {
-				"options" : {
-					"prop" : "pkg.version"
-				}
-			}
-		},
-
-		"json-replace" : {
-			"options" : {
-				"space" : "\t"
-			},
-			"package.json" : {
-				"options" : {
-					"replace" : {
-						"version" : "<%= pkg.version %>",
-						"devDependencies" : UNDEFINED
-					}
-				},
-				"files" : {
-					"<%= build.dist %>/package.json" : "<%= build.dist %>/package.json"
-				}
-			},
-			"bower.json" : {
-				"options" : {
-					"replace" : {
-						"version" : "<%= pkg.version %>"
-					}
-				},
-				"files" : {
-					"<%= build.dist %>/bower.json" : "<%= build.dist %>/bower.json"
-				}
-			}
-		},
-
-		"git-dist" : {
-			"all" : {
-				"options" : {
-					"branch" : "dist",
-					"dir" : "<%= build.dist %>",
-					"message" : "<%= pkg.name %> - <%= pkg.version %>",
-					"config" : {
-						"user.name" : UNDEFINED,
-						"user.email" : UNDEFINED
-					}
-				}
-			}
-		},
-
 		"buster" : {
 			"all": {}
 		}
 	});
 
 	grunt.loadNpmTasks("grunt-contrib-jshint");
-	grunt.loadNpmTasks("grunt-contrib-copy");
-	grunt.loadNpmTasks("grunt-contrib-clean");
-	grunt.loadNpmTasks("grunt-git-describe");
-	grunt.loadNpmTasks("grunt-git-dist");
-	grunt.loadNpmTasks("grunt-json-replace");
 	grunt.loadNpmTasks("grunt-buster");
 	grunt.registerTask("test", [ "buster" ]);
-	grunt.registerTask("dist", [ "clean", "git-dist:*:clone", "copy", "git-describe", "json-replace", "git-dist:*:commit", "git-dist:*:push" ]);
+	grunt.registerTask("default", [ "jshint", "test" ]);
 };
